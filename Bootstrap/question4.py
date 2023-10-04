59e015da6d01ec60491227e56bba40d0bc526e21
ArindamVerma02 <arindamverma02@gmail.com>
2023-10-04 21:51:21 +0530

    question4

---
+++ Bootstrap/question4.py
@@ -0,0 +1,9 @@
+n=0
+input(int("Enter the no. of rows: "))
+no=n-2
+cr=0
+cc=0
+ch=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range (n-2):
+    if cc==0:
+        print("*", end=" ")
